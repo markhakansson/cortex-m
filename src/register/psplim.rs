99157aa1cb7f9372fdc4a1288d9d3b57e55a9fde
Mark Hakansson <mark.hakansson@live.se>
2021-02-03 08:38:01 +0100

    Add klee feature support

--- src/register/psplim.rs
+++ src/register/psplim.rs
@@ -1,9 +1,17 @@
 //! Process Stack Pointer Limit Register
 
 /// Reads the CPU register
+#[cfg(not(feature = "klee-analysis"))]
 #[inline]
 pub fn read() -> u32 {
     call_asm!(__psplim_r() -> u32)
+}
+#[cfg(feature = "klee-analysis")]
+#[inline]
+pub fn read() -> u32 {
+    let mut r: u32 = unsafe { core::mem::MaybeUninit::uninit().assume_init() };
+    klee_make_symbolic!(&mut r, "PSPLIM_R");
+    r
 }
 
 /// Writes `bits` to the CPU register
